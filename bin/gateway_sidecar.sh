9afb5f05b11b12305dfaafc7b5e86c67a7866fdd
David Young <davidy@funkypenguin.co.nz>
2022-05-09 13:15:49 +1200

    Apply FORWARD rules earlier on
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- bin/gateway_sidecar.sh
+++ bin/gateway_sidecar.sh
@@ -10,6 +10,15 @@ cat /config/settings.sh
 
 if [[ -n "$VPN_INTERFACE" ]]; then
   # Create all NAT rules
+
+  # Add forwarding rules first, since this can take a loooooong time to run through!
+  iptables  -I FORWARD -i "$VPN_INTERFACE" -p tcp -d "${VXLAN_IP_NETWORK}.0.0/16" \
+            -m state --state NEW,ESTABLISHED,RELATED \
+            -j ACCEPT
+  iptables  -I FORWARD -i "$VPN_INTERFACE" -p udp -d "${VXLAN_IP_NETWORK}.0.0/16" \
+            -m state --state NEW,ESTABLISHED,RELATED \
+            -j ACCEPT
+
   PORT=1024 # starting port
   for THIRD in {0..255}
   do
@@ -21,21 +30,12 @@ if [[ -n "$VPN_INTERFACE" ]]; then
     iptables  -t nat -A PREROUTING -p tcp -i "$VPN_INTERFACE" \
               --dport $PORT  -j DNAT \
               --to-destination "${VXLAN_IP_NETWORK}.${THIRD}.${FOURTH}:${PORT}"
-    # iptables  -t nat -A PREROUTING -p udp -i "$VPN_INTERFACE" \
-    #           --dport $PORT  -j DNAT \
-    #           --to-destination "${VXLAN_IP_NETWORK}.${THIRD}.${FOURTH}:${PORT}"            
-    # iptables  -A FORWARD -p tcp -d "${VXLAN_IP_NETWORK}.${THIRD}.${FOURTH}" \
-    #           --dport "$PORT" -m state --state NEW,ESTABLISHED,RELATED \
-    #           -j ACCEPT
-    # iptables  -A FORWARD -p udp -d "${VXLAN_IP_NETWORK}.${THIRD}.${FOURTH}" \
-    #           --dport "$PORT" -m state --state NEW,ESTABLISHED,RELATED \
-    #           -j ACCEPT            
+    iptables  -t nat -A PREROUTING -p udp -i "$VPN_INTERFACE" \
+              --dport $PORT  -j DNAT \
+              --to-destination "${VXLAN_IP_NETWORK}.${THIRD}.${FOURTH}:${PORT}"        
     ((PORT=PORT+1))
     done
   done
-  iptables  -A FORWARD -i "$VPN_INTERFACE" -p tcp -d "${VXLAN_IP_NETWORK}.0.0/16" \
-            -m state --state NEW,ESTABLISHED,RELATED \
-            -j ACCEPT  
 fi
 
 echo "job done, going to sleep now..."
